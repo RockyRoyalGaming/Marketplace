ca8a0f0981b5ade900cdb375004885ddda0dd611
RockyRoyalGaming <xyztoonz@gmail.com>
2026-02-10 14:12:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,124 +1,94 @@
 // --- 1. DATABASE (Items List) ---
 const items = [
-         {
-        id: 14,  // Number badhate rahein (pichla 14 tha to ye 15)
+    {
+        id: 14,
         title: "Actions & Stuff 1.9.1",
-        category: "texture", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/bb31b676-ab14-4019-b302-8c0069ddd36b/actionsandstuff_Thumbnail_0.jpg", // Photo ka link
+        category: "texture",
+        image: "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/bb31b676-ab14-4019-b302-8c0069ddd36b/actionsandstuff_Thumbnail_0.jpg",
         description: "The Animation Pack You Didn't Know You Needed: Bring your world to life with new animations, particles, textures, and more!",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
-            { type: "Texture", url: "https://devuploads.com/2y27j0ya5mb8", icon: "fa-image" },
+            { type: "Texture", url: "https://devuploads.com/2y27j0ya5mb8", icon: "fa-image" }, // <--- Yahan Comma lagaya hai
             { type: "Skin", url: "https://devuploads.com/m5ivdmhn4v2j", icon: "fa-tshirt" }
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-         {
-        id: 13,  // Number badhate rahein (pichla 14 tha to ye 15)
+    {
+        id: 13,
         title: "Fungus Infection Add-On",
-        category: "addon", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://i.imgur.com/DygmgaK.jpeg", // Photo ka link
-        description: "The Fungus Infection Add-On can take over your Survival World! It Includes: (- 20+ Fungus Monsters with evolutions, - 3 Monster evolution stages, - The Fungus outbreak Biome that infect Vanilla blocks, - New infected Fungal Blocks, Plants, and Trees, - New Fungus Themed Armor Sets and Weapons and Items) Add-On’s make your world more fun!",
+        category: "addon",
+        image: "https://i.imgur.com/DygmgaK.jpeg",
+        description: "The Fungus Infection Add-On can take over your Survival World! Includes 20+ Fungus Monsters, Biomes, and Items.",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "Addon", url: "https://devuploads.com/e85g5tuj06kw", icon: "fa-puzzle-piece" }
-            
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-         {
-        id: 12,  // Number badhate rahein (pichla 11 tha to ye 12)
+    {
+        id: 12,
         title: "PETS Add-On",
-        category: "addon", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://i.imgur.com/VMRhvz0.jpeg", // Photo ka link
-        description: "Love brainrot Add-Ons? In this pack we add T.O.N.S of them as PETS on YOUR world! Craft 50+ brainrot pets that EVOLVE, BATTLE, and that you can RIDE! (- 96 Brainrot Pets! , - Use Add-Ons on YOUR worlds! , - Compatible with other Add-Ons)",
+        category: "addon",
+        image: "https://i.imgur.com/VMRhvz0.jpeg",
+        description: "Craft 50+ brainrot pets that EVOLVE, BATTLE, and that you can RIDE! (- 96 Brainrot Pets!)",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "Addon", url: "https://devuploads.com/u5qxftp31ryu", icon: "fa-puzzle-piece" }
-            
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-         {
-        id: 11,  // Number badhate rahein (pichla 11 tha to ye 12)
+    {
+        id: 11,
         title: "Health Bars 1.1 Add-On",
-        category: "addon", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://i.postimg.cc/T2zPVCRf/image-12-(30).jpg", // Photo ka link
-        description: "Ever wondered how tough a Warden really is? Wonder no more! With the Health Bars Add-On, every mob and player gets their very own Health Bar. Gauge the fight and strategise your next move at a glance!",
+        category: "addon",
+        image: "https://i.postimg.cc/T2zPVCRf/image-12-(30).jpg",
+        description: "With the Health Bars Add-On, every mob and player gets their very own Health Bar.",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "Addon", url: "https://linkpays.in/MeNBy", icon: "fa-puzzle-piece" }
-            
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-         {
-        id: 10,  // Number badhate rahein (pichla 9 tha to ye 10)
+    {
+        id: 10,
         title: "Forest Craft Add-On 4.0",
-        category: "addon", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://i.postimg.cc/5yQ7JfGm/image-12-(29).jpg", // Photo ka link
-        description: "Forest Craft Add-On 4.0 adds 30 Trees to your Survival world. Chop new trees, harvest fruit, cook new foods and build with 28 new wood types!",
+        category: "addon",
+        image: "https://i.postimg.cc/5yQ7JfGm/image-12-(29).jpg",
+        description: "Forest Craft Add-On 4.0 adds 30 Trees to your Survival world. Chop new trees, harvest fruit, and build with 28 new wood types!",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "Addon", url: "https://linkpays.in/d6yftsfw", icon: "fa-puzzle-piece" }
-            
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-         {
-        id: 9,  // Number badhate rahein (pichla 9 tha to ye 10)
+    {
+        id: 9,
         title: "Stranger Things",
-        category: "world", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://i.postimg.cc/SK7WMpy3/image_12_(28).jpg", // Photo ka link
-        description: "*sniff, sniff* Do you smell waffles? It must be Netflix's Stranger Things DLC, just in time for the epic series finale! Join “The Party” and play as your favorite character, like Will, Dustin, Mike, or Eleven. characters' unique abilities will help you solve puzzles and vanquish foes. Hop on your bike and head to the Dressing Room for your free Hellfire Club T-shirt!",
+        category: "world",
+        image: "https://i.postimg.cc/SK7WMpy3/image_12_(28).jpg",
+        description: "Join The Party and play as your favorite character to solve puzzles and vanquish foes.",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "World", url: "https://linkpays.in/nXrWN8y", icon: "fa-globe" },
             { type: "Skin", url: "https://www.mediafire.com/file/ok4rs6ns52z7hpn/Stranger_Things_%2528skin_pack%2529_RockyRoyalGaming.mcpack/file", icon: "fa-tshirt" }
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-         {
-        id: 8,  // Number badhate rahein (pichla 7 tha to ye 8)
+    {
+        id: 8,
         title: "Angry Birds",
-        category: "world", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/203ff9c0-97e4-4c61-9518-54bf6c53208f/angrybirds_Thumbnail_0.jpg", // Photo ka link
-        description: "The Pigs are back to their old tricks, and it's up to you to stop them! Playing as Red, Chuck, Bomb, and other members of The Flock, your mission is to rescue your friends, save the eggs, and stop the pesky Pigs' grand plans. With each rescued bird, you'll unlock a new playable character with unique skills to help you pop more Pigs and topple more towers!",
+        category: "world",
+        image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/203ff9c0-97e4-4c61-9518-54bf6c53208f/angrybirds_Thumbnail_0.jpg",
+        description: "Rescue your friends, save the eggs, and stop the pesky Pigs' grand plans.",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "World", url: "https://linkpays.in/YsHu", icon: "fa-globe" },
             { type: "Skin", url: "https://gofile.io/d/UQdYoa", icon: "fa-tshirt" }
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
-        {
-        id: 7,  // Number badhate rahein (pichla 6 tha to ye 7)
+    {
+        id: 7,
         title: "Bluey's House",
-        category: "world", // Options: 'addon', 'world', 'skin', 'texture', 'mashup'
-        image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/345ffd83-2bc6-4990-86a5-6e79888ab6d4/BLUEY_Thumbnail_0.jpg", // Photo ka link
-        description: "Get ready for heaps of fun with Bluey, Bingo, Mum, and Dad! Step inside Bluey’s Family home and play games like I-Spy, Hide and Seek, and Ragdoll. Find the Family’s favourite items to unlock even more games and surprises!",
+        category: "world",
+        image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/345ffd83-2bc6-4990-86a5-6e79888ab6d4/BLUEY_Thumbnail_0.jpg",
+        description: "Step inside Bluey’s Family home and play games like I-Spy, Hide and Seek, and Ragdoll.",
         links: [
-            // Agar Addon hai to bas ye line rakhein:
             { type: "World", url: "https://linkpays.in/HOApOgEm", icon: "fa-globe" }
-            
-            // Agar World + Skin hai to niche wali line bhi use karein:
-            // { type: "Skin", url: "LINK_HERE", icon: "fa-tshirt" }
         ]
     },
     {
         id: 1,
-        title: "How to Train Your Dragon Add-On",
+        title: "How to Train Your Dragon",
         category: "addon",
         image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/ccca9e5a-b363-47d4-b75d-b8622d28cb15/zenith_thumbnail_0.jpg",
-        description: "Discover Toothless and other beloved dragons in your Minecraft Worlds! Become a dragon rider with this Add-On based on Universal Pictures’ live-action reimagining of DreamWorks Animation’s How to Train Your Dragon. Take off into the skies, learn each dragon's personality, and bond with them to unlock unique abilities. Embark on an iconic adventure with your new dragon!",
-        // Addon: Sirf ek button
+        description: "Discover Toothless and other beloved dragons in your Minecraft Worlds!",
         links: [
             { type: "Addon", url: "https://linkpays.in/fXMhJX", icon: "fa-puzzle-piece" }
         ]
@@ -128,8 +98,7 @@ const items = [
         title: "Dragon Ball Z",
         category: "world",
         image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/c3f0a98e-f39b-4f40-944e-854395bb2f59/dragon_ball_z_thumbnail_0.jpg",
-        description: "Battle as your favorite Dragon Ball Z character in this explosive DLC! Master iconic moves, battle in legendary arenas from the series, and see who dominates the battlefield.",
-        // World: World + Skin Buttons
+        description: "Battle as your favorite Dragon Ball Z character in this explosive DLC!",
         links: [
             { type: "World", url: "https://linkpays.in/z3ehlv", icon: "fa-globe" },
             { type: "Skin", url: "https://gofile.io/d/LmRxXL", icon: "fa-tshirt" }
@@ -138,10 +107,9 @@ const items = [
     {
         id: 3,
         title: "STAR WARS",
-        category: "mashup", /* <-- Ye raha Mashup Item */
+        category: "mashup",
         image: "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/a69407fb-6802-4036-bd95-ecd431be7987/Star_Wars_Thumbnail_0.jpg",
-        description: "Complete Star Wars experience including World, Skins, and Textures.Explore the galaxy in this epic STAR WARS Mash-up, featuring characters and locations from the original trilogy and The Mandalorian series. This is the way!",
-        // Mashup: Mashup Pack Button
+        description: "Complete Star Wars experience including World, Skins, and Textures.",
         links: [
             { type: "Mashup Pack", url: "https://linkpays.in/cfcDc", icon: "fa-layer-group" }
         ]
@@ -151,8 +119,7 @@ const items = [
         title: "Realism Visuals",
         category: "texture",
         image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/cff099cb-5d80-4743-ba4e-a149796db238/Realism_Visuals_Thumbnail_0.jpg",
-        description: "Fine-tuned for vanilla. Realistic shading and custom player animations. + Real lighting, water, and block materials +Vibrant biome shades +Realistic player animations +Complementary to vanilla textures",
-        // Texture: Texture Button
+        description: "Fine-tuned for vanilla. Realistic shading and custom player animations.",
         links: [
             { type: "Texture", url: "https://linkpays.in/ToXtj", icon: "fa-image" }
         ]
@@ -162,8 +129,7 @@ const items = [
         title: "Bee Farmer",
         category: "skin",
         image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/f6efebd9-5e5a-4452-a5da-4116164af024/BeeFarmer_Thumbnail_0.jpg",
-        description: "Ready to harvest honey! ,From hardworking farmers to buzzing bees, this collection brings the countryside to life with bright colors and cheerful vibes.,- Includes 12 unique skins, one is free to try!,- Mix of farmers, beekeepers, and even a friendly bee or two.,- Perfect for building farms, tending bees, or just hanging out with friends.",
-        // Skin: Skin Button
+        description: "Includes 12 unique skins, one is free to try!",
         links: [
             { type: "Skin", url: "https://linkpays.in/vQHtOc", icon: "fa-tshirt" }
         ]
@@ -171,9 +137,9 @@ const items = [
     {
         id: 6,
         title: "Dinosaur Biomes",
-        category: "mashup", /* <-- Ek aur Mashup example */
+        category: "mashup",
         image: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/8d5f7d0a-1830-4848-81c1-42886404987d/DinosaurMashup_Thumbnail_0.jpg",
-        description: "- Custom Biomes themed from the Jurassic age!- Dinosaurs are tamable and ridable!- 15+ New Dinosaurs- 18+ New utility tools- 7 Jurassic aged Weapons- 7 Jurassic aged Armor Sets- 9 dinosaur themed materials- A new wandering trader, This item uses random start areas for any new world. Endless possibilities!",
+        description: "Custom Biomes themed from the Jurassic age! Dinosaurs are tamable and ridable!",
         links: [
             { type: "Mashup Pack", url: "https://linkpays.in/ikxkR1", icon: "fa-layer-group" }
         ]
